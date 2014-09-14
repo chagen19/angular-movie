e9ddc9ea48d615103f1a0a577cf09b2c1990c6bf
chagen19 <chagen19@gmail.com>
2014-09-14 16:44:26 -0500

    Initial commit

---
+++ app/js/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('movieApp', [
+  'ngRoute',
+  'ngAnimate',
+  'movieControllers'
+]).
+config(['$routeProvider', 
+  function($routeProvider) {
+    $routeProvider.when('/search/:movieName', {
+      templateUrl: 'partials/movies.html', 
+      controller: 'SearchCtrl'
+    });
+     $routeProvider.when('/favorites', {
+      templateUrl: 'partials/movies.html', 
+      controller: 'FavoritesCtrl'
+    });
+    $routeProvider.when('/details/:movieId', {
+      templateUrl: 'partials/movieDetail.html', 
+      controller: 'DetailCtrl'
+    });
+    $routeProvider.otherwise({
+      redirectTo: '/'
+    });
+}]).run(["$rootScope", "$http", 
+         function ($rootScope, $http) {
+         $http.get("https://api.themoviedb.org/3/configuration?api_key=013eff1b8075d646416de6ec45620619").success(function(data) {
+  			$rootScope.url = data.images.base_url;
+        $rootScope.tid=0;
+  		});	
+
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/* Controllers */
+
+var movieControllers = angular.module('movieControllers', [])
+
+.controller("SearchCtrl", ['$rootScope', '$scope', '$http', '$routeParams',
+	function($rootScope, $scope, $http, $routeParams) {
+		console.log("here");
+		$http.get("https://api.themoviedb.org/3/search/movie?api_key=013eff1b8075d646416de6ec45620619&query=" + $routeParams.movieName).success(function(data) {
+			$scope.results = data.results;
+			$scope.total = data.total_results;
+			$scope.orderProp = 'release_date';
+		});
+		
+		$scope.addFavorite = function(fav) {
+			console.log("Adding Favorite");
+			$http.post("http://localhost:3000/profile/fav/1", fav).success(function(data) {
+				console.log("Favorite Added");
+			});
+		};
+	}])
+.controller("FavoritesCtrl", ['$scope', '$http', '$routeParams',
+	function($scope, $http, $routeParams) {
+		$scope.results = [];
+		$http.get("http://localhost:3000/profile/1").success(function(data) {
+			var favorites = data.favorites;
+			angular.forEach(favorites, function(fav) {
+				console.log(fav['title'])
+				fav.favorite = true;
+				this.push(fav);
+			}, $scope.results);
+			$scope.page = "Favorites";
+			$scope.total = favorites.length;
+			$scope.orderProp = 'release_date';
+		});
+	}])
+.controller("DetailCtrl", ['$rootScope', '$scope', '$http', '$routeParams', '$sce',
+	function($rootScope, $scope, $http, $routeParams, $sce) {
+		$http.get("https://api.themoviedb.org/3/movie/" + $routeParams.movieId + "?api_key=013eff1b8075d646416de6ec45620619&append_to_response=credits,trailers").success(function(data) {
+		$scope.movie = data;
+
+		$scope.setTrailer = function(value) {
+			console.log("Setting trailer to index " + value);
+			$scope.yturl = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + data.trailers.youtube[value].source + "?enablejsapi=0");
+			$scope.trailer = data.trailers.youtube[value];
+		};
+		$scope.setTrailer(0);
+		$rootScope.movieId = data.id;
+		});
+	}]);
