c33ff253d9a7c9c6a5a9e30eaa8ff9738a212722
chagen19 <chagen19@gmail.com>
2014-09-21 22:11:11 -0500

    More restructoring of the project

---
+++ app/js/modules/favorites.js
@@ -0,0 +1,65 @@
+'use strict';
+
+angular.module('movieApp.favorites', [
+	'ngRoute',
+	'movieApp.favorites.controllers'
+])
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/favorites', {
+      templateUrl: 'views/movies.html',
+      controller: 'FavoritesCtrl'
+    });
+}]);
+
+angular.module('movieApp.favorites.controllers', [
+	'movieApp.favorites.services',
+	'ngAnimate',
+  	'iso.directives'
+]).controller('FavoritesCtrl', ['$scope', '$timeout', 'favoriteService',
+	function($scope, $timeout, favoriteService) {
+		$scope.results = [];
+		favoriteService.getFavorites().success(function(data) {
+			var favorites = data.favorites;
+			angular.forEach(favorites, function(fav) {
+				console.log(fav['title']);
+				fav.favorite = true;
+				fav.profileId = data._id;
+				this.push(fav);
+			}, $scope.results);
+			$scope.page = "Favorites";
+			$scope.total = favorites.length;
+			$scope.orderProp = 'release_date';
+		});
+
+		$scope.removeFavorite = function(fav) {
+			favoriteService.removeFavorite(fav);
+			var index = $scope.results.indexOf(fav);
+			$scope.results.splice(index, 1);
+			$scope.$broadcast('iso-init', {name: null, params: null});
+		};
+
+}]);
+
+angular.module('movieApp.favorites.services', [
+])
+.factory('favoriteService', ['$rootScope', '$http', function($rootScope, $http) {
+
+	var factory = {};
+	factory.getFavorites = function() {
+		return $http.get("http://localhost:3000/profile/" + $rootScope.profileId);
+	};
+
+	factory.removeFavorite = function(fav) {
+		console.log("Removing favorite from profile " + fav.profileId);
+		$http.delete("http://localhost:3000/profile/" + fav.profileId + "/favorite/" + fav._id).success(function(data) {
+			console.log(fav.title + " removed from favorites!");
+		});
+	};
+	factory.addFavorite = function(fav) {
+		fav.source = 'themoviedb';
+		$http.put("http://localhost:3000/profile/54166d47c0edc80000a81de4", fav).success(function(data) {
+			console.log(fav.title + " added to favorites!");
+		});
+	};
+	return factory;
+}]);
---
+++ app/js/modules/themoviedb.js
@@ -0,0 +1,102 @@
+'use strict';
+
+angular.module('movieApp.theMovieDB', [
+	'ngRoute',
+	'movieApp.theMovieDB.controllers'
+])
+.config(['$routeProvider', function($routeProvider){
+	 $routeProvider.when('/search/:movieName', {
+      templateUrl: 'views/movies.html',
+      controller: 'SearchCtrl'
+    });
+		$routeProvider.when('/details/:movieId', {
+				templateUrl: 'views/movieDetail.html',
+				controller: 'DetailCtrl'
+			});
+}]);
+
+angular.module('movieApp.theMovieDB.controllers', [
+	'movieApp.theMovieDB.services',
+	'movieApp.favorites.services',
+	'movieApp.shared.directives',
+	'ngAnimate',
+  'iso.directives'
+])
+.controller('SearchCtrl', ['$scope', '$timeout', 'theMovieDBService', 'favoriteService',
+	function($scope, $timeout, theMovieDBService, favoriteService) {
+		theMovieDBService.getMovies().success(function(data) {
+			$scope.results = data.results;
+			$scope.total = data.total_results;
+			$scope.orderProp = 'release_date';
+		});
+
+		$scope.addFavorite = function(fav) {
+			fav.source = 'themoviedb';
+			fav.favorite=true;
+			favoriteService.addFavorite(fav);
+		};
+}])
+.controller('DetailCtrl', ['$rootScope', '$scope', '$sce', '$timeout', 'theMovieDBService',
+	function($rootScope, $scope, $sce, $timeout, theMovieDBService) {
+		theMovieDBService.getMovieDetails().success(function(data) {
+			$scope.movie = data;
+			$scope.setTrailer(0);
+			$rootScope.movieId = data.id;
+
+			<!-- Fix to delay isotop until images are loaded -->
+			$timeout(function() {
+				console.log('Re-initiating isotope');
+				$scope.$broadcast('iso-init', {name: null, params: null});
+			}, 700);
+		});
+	$scope.setTrailer = function(index) {
+		var data = $scope.movie;
+		var trailer = data.trailers.youtube[index];
+		if(null != data.trailers.youtube[index] || null) {
+			console.log("Setting trailer to index " + index);
+			$scope.yturl = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + data.trailers.youtube[index].source + "?enablejsapi=0");
+			$scope.trailer = data.trailers.youtube[index];
+		}
+	};
+}]);
+
+angular.module('movieApp.theMovieDB.services', [
+])
+.factory('theMovieDBService', ['$http', '$routeParams', 'apiUrl', 'apiKey', function($http, $routeParams, apiUrl, apiKey) {
+	return {
+		getMovies : function() {
+			return $http.get(apiUrl + "/search/movie?api_key=" + apiKey + "&query=" + $routeParams.movieName);
+		},
+		getMovieDetails : function() {
+			return $http.get(apiUrl + "/movie/" + $routeParams.movieId + "?api_key=" + apiKey + "&append_to_response=credits,trailers");
+		},
+		getConfigurationData : function() {
+			return $http.get(apiUrl + "/configuration?api_key=" + apiKey);
+		}
+	}
+}]);
+
+angular.module('movieApp.shared.directives', [
+])
+.directive('currentItem', ['$timeout', function($timeout) {
+
+function link(scope, element, attrs) {
+    scope.$watch(attrs.currentItem, function(value) {
+    	if(value == scope.results.length-1) {
+        	<!-- Fix to delay isotop until images are loaded -->
+			$timeout(function() {
+				console.log('Re-initiating isotope');
+				scope.$emit('iso-init', {name: null, params: null});
+			}, 500);
+        }
+    });
+
+}
+
+ return {
+      link: link
+    };
+
+}])
+.value('apiUrl', 'https://api.themoviedb.org/3')
+.value('apiKey', '013eff1b8075d646416de6ec45620619');
