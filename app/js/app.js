3bed7e6c4275ae970d6d0ac73a5252d33b412c82
chagen19 <chagen19@gmail.com>
2014-09-21 22:10:51 -0500

    More restructoring of the project

--- app/js/app.js
+++ app/js/app.js
@@ -5,24 +5,23 @@
 angular.module('movieApp', [
   'ngRoute',
   'movieApp.favorites',
-  'movieApp.search',
-  'movieApp.details',
+  'movieApp.theMovieDB',
   'iso.directives'
 ]).
 config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({
       redirectTo: '/'
     });
-}]).run(['$rootScope', '$http', '$location',
-    function ($rootScope, $http, $location) {
+}]).run(['$rootScope', '$http', '$location', 'theMovieDBService',
+    function ($rootScope, $http, $location, theMovieDBService) {
       console.log("Getting configuration data");
-        $http.get("https://api.themoviedb.org/3/configuration?api_key=013eff1b8075d646416de6ec45620619").success(function(data) {
+        theMovieDBService.getConfigurationData().success(function(data) {
   			$rootScope.url = data.images.base_url;
         $rootScope.tid=0;
         $rootScope.profileId = 1;
-        
+
         $rootScope.go = function(path) {
           $location.path(path);
         };
-  	});	
+  	});
 }]);
